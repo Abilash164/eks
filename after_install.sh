c29a9dcc4947cbfa36029b2d79810a062a1728b0
Abilash164 <129362119+Abilash164@users.noreply.github.com>
2023-05-16 07:12:02 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,2 +1,2 @@
-kubectl apply -f deplyoment.yml
-kubectl apply -f service.yml
+aws eks --region  p-south-1 describe-cluster  --name eks --query cluster.status
+aws eks --region  ap-south-1 update-kubeconfig --name eks
