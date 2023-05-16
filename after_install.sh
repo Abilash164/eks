97198ad81315894ad009f8877a853dfc0b929a31
Abilash164 <129362119+Abilash164@users.noreply.github.com>
2023-05-16 08:34:42 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,2 +1,2 @@
-aws eks --region ap-south-1 describe-cluster  --name wwe-test --query cluster.status
-aws eks --region ap-south-1 update-kubeconfig --name wwe-test 
+aws eks --region ap-south-1 describe-cluster  --name eks-test --query cluster.status
+aws eks --region ap-south-1 update-kubeconfig --name eks-test 
