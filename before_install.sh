c1b740ff5011327fb7ad45b72ea8ec80e4fd92bc
Abilash164 <129362119+Abilash164@users.noreply.github.com>
2023-05-16 09:24:46 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,9 @@
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/arm64/kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl.sha256
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/arm64/kubectl.sha256
 sha256sum -c kubectl.sha256
 openssl sha1 -sha256 kubectl
+chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
